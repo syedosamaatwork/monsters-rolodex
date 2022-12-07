aa8a217ab1895bed7a5f6d20bb94006a1dd58c98
Get Licensed <getlicensed@Gets-MacBook-Pro-3.local>
2022-12-07 20:39:23 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { id: '7f98asd79f', name: 'Frank' },
+        { id: '7f98asd74f', name: 'James' },
+        { id: '7f98asd75f', name: 'Eddie' },
+        { id: '7f98asd76f', name: 'Henry' }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <div key={monster.id}><h1>{monster.name}</h1></div>
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default App;
